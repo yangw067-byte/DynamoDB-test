205c0d08dbbb685d8e5963e43ab0a47f22c65828
yangw067-byte <wei_yang80@yahoo.com>
2026-05-29 14:36:17 -0400

    fix the backend

--- provider.tf
+++ provider.tf
@@ -1,6 +1,9 @@
 terraform {
   backend "s3" {
-    use_lockfile   = true # Enables native S3 state locking
+    use_lockfile = true # Enables native S3 state locking
+    bucket       = ""
+    key          = ""
+    region       = ""
   }
 }
 
